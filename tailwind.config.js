7d9ad3b119c8d59008a221b7d116849810e313b2
ilyass anida <anidailyass@gmail.com>
2024-11-25 17:30:23 +0100

    #5 Create the players cards with static data.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "secondary": "#393218",
+      }
+    },
   },
   plugins: [],
 }
