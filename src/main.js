199fbc549704df885a9401d6f0735fab3f44b455
ilyass anida <anidailyass@gmail.com>
2024-11-27 11:40:39 +0100

    #12 Create the field empty cards design.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
-const positions = ["ST", "LW", "RW", "CB", "RB", "LB", "GK"];
+const positions = ["ST", "LW", "RW", "CM", "CB", "RB", "LB", "GK"];
+const allFieldPositions = [["LW", "ST", "RW"], ["CM", "CM", "CM"], ["LB", "CB", "CB", "RB"], ["GK"]];
+
 let substitutions = [];
 let fieldPlayers = [];
+
+const formationLines = document.querySelectorAll("#formation .line");
+
+
+showSubstitutionsPlayers();
+showEmptyFieldCards();
+
 
 async function loadPlayers () {
     if (substitutions.length > 0) return;
@@ -18,6 +27,16 @@ async function showSubstitutionsPlayers() {
         let card = substitutionCard(player);
         container.appendChild(card);
     })
+}
+
+function showEmptyFieldCards() {
+    formationLines.forEach((line, lineIndex) => {
+        line.innerHTML = "";
+        for (let i = 0; i < allFieldPositions[lineIndex].length; i++) {
+            let emptyCard = emptyFieldCard(allFieldPositions[lineIndex][i]);
+            line.appendChild(emptyCard);
+        }
+    });
 }
 
 function substitutionCard({name, position, rating, photo:playerImage, logo:clubImage, flag:countryImage}) {
@@ -52,4 +71,37 @@ function substitutionCard({name, position, rating, photo:playerImage, logo:clubI
     return tempDiv.firstElementChild;
 }
 
-showSubstitutionsPlayers();
+
+function playerFieldCard() {
+    let tempDiv = document.createElement("div");
+
+    tempDiv.innerHTML = `
+    <div class="relative card w-22">
+        <img src="../assets/images/card.webp" alt="Player Card">
+        <img src="https://cdn.sofifa.net/players/020/801/25_120.png" alt="Cristiano Ronaldo" class="absolute bottom-[36%] left-1/2 -translate-x-1/2 w-3/5">
+        <div class="flex flex-col items-center absolute font-bold top-[20%] left-[12%] text-secondary">
+            <div class="text-xs">87</div>
+            <span class="text-[.45rem]">CF</span>
+        </div>
+        <div class="flex items-center text-secondary flex-col left-0 top-[60%] absolute w-full mt-1.5">
+            <div class="text-[.45rem] text-center font-bold">Cristiano Ronaldo</div>
+            <div class="flex w-full items-center justify-center gap-2">
+                <img class="w-4" src="https://cdn.sofifa.net/meta/team/2506/120.png" alt="Club">
+                <img class="h-2.5" src="https://cdn.sofifa.net/flags/pt.png" alt="Club">
+            </div>
+        </div>
+    </div>`
+
+    return tempDiv.firstElementChild;
+}
+
+function emptyFieldCard(id) {
+    let tempDiv = document.createElement("div");
+
+    tempDiv.innerHTML = `
+                    <div id=${id} class="relative card w-22">
+                        <img src="../assets/images/card-informe.webp" alt="Player Card">
+                    </div>
+                    `
+    return tempDiv.firstElementChild;
+}
